98175124dc59c17a0ff6da5312f6371e75a70ae8
Edward Chung <edwardchung2024@gmail.com>
2023-02-15 11:42:05 -0500

    data validator

--- index.js
+++ index.js
@@ -1,19 +1,59 @@
-import express from 'express';
-
+const express = require("express");
+const fs = require("fs");
+const data = fs.readFileSync("data.json");
+const Validator = require('jsonschema').Validator;
 const app = express();
-
 const PORT = 5000;
+
+const medicalBills = JSON.parse(data);
+const v = new Validator();
+
+const billSchema = {
+    "id": "Medical Bill",
+    "type": "object",
+    "properties": {
+        "name": {"type": "string"},
+        "address": {"type": "string"},
+        "hospital": {"type": "string"},
+        "date": {"type": "string"},
+        "bill": {"type": "number"}
+    },
+    "required": ["name", "address", "hospital", "date", "bill"]
+};
+
+v.addSchema(billSchema, 'Medical Bill');
 
 app.listen(
     PORT,
-    () => console.log(`Server is running on: ${5000}`)
+    () => console.log(`Server is running on port: ${5000}`)
 );
 
 app.get('/items', (req, res) => {
-    res.send("Hello, This is a successful GET Request")
+    res.json(medicalBills);
 });
 
-app.post('/postRequest', (req, res) => {
-    res.send("Hello, This is a successful POST Request")
+const data1 = {
+    "name": "11",
+    "address": "22",
+    "hospital": "33",
+    "date": "44",
+    "bill": 55
+}
+
+// console.log(v.validate(data1, billSchema).valid)
+
+app.post('/items', (req, res) => {
+    if (v.validate(data1, billSchema).valid) {
+        medicalBills.push(data1);
+        const newData = JSON.stringify(medicalBills);
+        fs.writeFile("data.json", newData, (err) => {
+            if (err) throw err;
+            console.log("New data added");
+        })
+        res.json(JSON.parse(newData));
+    }
+    else {
+        res.status(400).json({ message: "Invalid medical bill"})
+    }
 });
 
